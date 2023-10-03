23981583a819c5fcd6b81996c750232d68ff82df
alchemistbg <alchemist_bg@abv.bg>
2023-10-03 20:28:14 +0200

    Add SQL code for the lesson 'More on the WHERE keyword'

---
+++ Section-02/14_-_More_on_the_Where_Keyword.sql
@@ -0,0 +1,8 @@
+-- More filtering with WHERE
+
+-- Equal operator
+SELECT name, area FROM cities WHERE area = 8223;
+
+-- Not Equal operators
+SELECT name, area FROM cities WHERE area != 8223;
+SELECT name, area FROM cities WHERE area <> 8223;
