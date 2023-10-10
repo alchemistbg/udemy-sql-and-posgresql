be740e393547e77a778baf6699b4bacc6f8cf761
alchemistbg <alchemist_bg@abv.bg>
2023-10-10 20:02:26 +0200

    Add SQL code for the coding exercise 'Testing Deletion Constrains'

---
+++ Section-03/40_Testing_Deletion_Constraints.sql
@@ -0,0 +1,29 @@
+-- Delete photos able
+DROP TABLE photos;
+
+-- Add ON DELETE CASCADE constraint
+CREATE TABLE photos (
+	id SERIAL PRIMARY KEY,
+	url VARCHAR(200),
+	user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+ 
+INSERT INTO photos (url, user_id)
+VALUES
+	('http:/one.jpg', 4),
+	('http:/two.jpg', 1),
+	('http:/25.jpg', 1),
+	('http:/36.jpg', 1),
+	('http:/754.jpg', 2),
+	('http:/35.jpg', 3),
+	('http:/256.jpg', 4);
+
+-- Check photos table
+SELECT * FROM photos;
+
+-- Delete user from users table
+DELETE FROM users WHERE id = 1;
+
+-- Check photos table
+SELECT * FROM photos;
+
