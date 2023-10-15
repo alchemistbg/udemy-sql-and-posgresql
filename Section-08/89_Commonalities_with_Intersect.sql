b163a49f90d49388415fcd192501b15bfb9d6a87
alchemistbg <alchemist_bg@abv.bg>
2023-10-15 19:41:58 +0200

    Add SQL code for the lesson 'Commonalities with INTERSECT'

---
+++ Section-08/89_Commonalities_with_Intersect.sql
@@ -0,0 +1,9 @@
+(SELECT * FROM products ORDER BY price DESC LIMIT 4)
+INTERSECT
+(SELECT * FROM products ORDER BY price/weight DESC LIMIT 4)
+
+
+
+(SELECT * FROM products ORDER BY price DESC LIMIT 4)
+INTERSECT ALL
+(SELECT * FROM products ORDER BY price/weight DESC LIMIT 4)
