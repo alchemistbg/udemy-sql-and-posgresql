33890f4572e6b2b3b9d926caca0afce5f5092d04
alchemistbg <alchemist_bg@abv.bg>
2023-11-14 20:24:27 +0100

    Add SQL code for the exercise 'Subquery Where'

---
+++ Section-09/Coding_Exercise_21_-_Subquery_Where.sql
@@ -0,0 +1,27 @@
+/*
+Subquery Where's
+
+Write a query that prints out the name and price of phones that have a price greater than the Samsung S5620 Monte.
+
+For reference, here is the phones table:
+
+    +-------------+--------------+-------+------------+
+    | name        | manufacturer | price | units_sold |
+    +-------------+--------------+-------+------------+
+    | N1280       | Nokia        | 199   | 1925       |
+    +-------------+--------------+-------+------------+
+    | Iphone 4    | Apple        | 399   | 9436       |
+    +-------------+--------------+-------+------------+
+    | Galaxy S    | Samsung      | 299   | 2359       |
+    +-------------+--------------+-------+------------+
+    | S5620 Monte | Samsung      | 250   | 2385       |
+    +-------------+--------------+-------+------------+
+    | N8          | Nokia        | 150   | 7543       |
+    +-------------+--------------+-------+------------+
+    | Droid       | Motorola     | 150   | 8395       |
+    +-------------+--------------+-------+------------+
+    | Wave S8500  | Samsung      | 175   | 9259       |
+    +-------------+--------------+-------+------------+
+*/
+
+SELECT name, price FROM phones WHERE price > (SELECT price FROM phones WHERE name = 'S5620 Monte');
