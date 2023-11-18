3846b91ca3b43d2536a02bee1772b798c41462fa
alchemistbg <alchemist_bg@abv.bg>
2023-11-18 20:53:07 +0100

    Add SQL code for the exercise 'FROM-less Select'

---
+++ Section-09/Coding_Exercise_23_-_From-less_Selects.sql
@@ -0,0 +1,35 @@
+/*
+From-less Selects
+
+Using only subqueries, print the max price, min price, and average price of all phones.  Rename each column to max_price, min_price, avg_price
+
+The result should look something like this:
+
+    +-----------+-----------+-----------+
+    | max_price | min_price | avg_price |
+    +-----------+-----------+-----------+
+    | ...       | ...       | ...       |
+    +-----------+-----------+-----------+
+
+For reference, here is the phones table:
+
+    +-------------+--------------+-------+------------+
+    | name        | manufacturer | price | units_sold |
+    +-------------+--------------+-------+------------+
+    | N1280       | Nokia        | 199   | 1925       |
+    +-------------+--------------+-------+------------+
+    | Iphone 4    | Apple        | 399   | 9436       |
+    +-------------+--------------+-------+------------+
+    | Galaxy S    | Samsung      | 299   | 2359       |
+    +-------------+--------------+-------+------------+
+    | S5620 Monte | Samsung      | 250   | 2385       |
+    +-------------+--------------+-------+------------+
+    | N8          | Nokia        | 150   | 7543       |
+    +-------------+--------------+-------+------------+
+    | Droid       | Motorola     | 150   | 8395       |
+    +-------------+--------------+-------+------------+
+    | Wave S8500  | Samsung      | 175   | 9259       |
+    +-------------+--------------+-------+------------+
+*/
+
+SELECT (SELECT MAX(price) from phones) AS max_price, (SELECT MIN(price) from phones) AS min_price, (SELECT AVG(price) from phones) AS avg_price;
