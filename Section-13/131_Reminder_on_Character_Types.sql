7050e1ad7211ff3c1e7479662f1f315f13766ee2
alchemistbg <alchemist_bg@abv.bg>
2023-11-23 20:39:19 +0100

    Add SQL code for the lecture 'Reminder on Character Types'

---
+++ Section-13/131_Reminder_on_Character_Types.sql
@@ -0,0 +1,17 @@
+SELECT ('Some_dummy_text'::CHAR(3));
+
+SELECT ('Some_dummy_text'::CHAR(5));
+
+SELECT ('Some_dummy_text'::CHAR);
+
+SELECT ('Some_dummy_text'::CHAR(20));
+
+SELECT ('Some_dummy_text'::VARCHAR(3));
+
+SELECT ('Some_dummy_text'::VARCHAR(5));
+
+SELECT ('Some_dummy_text'::VARCHAR);
+
+SELECT ('Some_dummy_text'::VARCHAR(20));
+
+SELECT ('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
