7402c1b67c5311e9aa37742513292ba1383e62c0
alchemistbg <alchemist_bg@abv.bg>
2023-11-24 20:24:27 +0100

    Add SQL code for the lecture 'Boolean Data Types'

---
+++ Section-13/132_Boolean_Data_Types.sql
@@ -0,0 +1,29 @@
+-- The following values are casted to TRUE
+SELECT ('true'::BOOLEAN);
+
+SELECT ('t'::BOOLEAN);
+
+SELECT ('y'::BOOLEAN);
+
+SELECT ('yes'::BOOLEAN);
+
+SELECT ('on'::BOOLEAN);
+
+SELECT (1::BOOLEAN);
+
+SELECT (42::BOOLEAN);
+
+-- The following values are casted to FALSE
+SELECT ('false'::BOOLEAN);
+
+SELECT ('f'::BOOLEAN);
+
+SELECT ('n'::BOOLEAN);
+
+SELECT ('no'::BOOLEAN);
+
+SELECT ('off'::BOOLEAN);
+
+SELECT (0::BOOLEAN);
+
+SELECT (null::NULL);
