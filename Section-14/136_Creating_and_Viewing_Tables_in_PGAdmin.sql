55e92813c711ad4c86ee5655c2060935e53588a4
alchemistbg <alchemist_bg@abv.bg>
2023-11-27 20:31:19 +0100

    Add SQL code for the lecture 'Creating and Viewing Tables in PGAdmin'

---
+++ Section-14/136_Creating_and_Viewing_Tables_in_PGAdmin.sql
@@ -0,0 +1,16 @@
+-- CREATE DATABASE test;
+
+-- CREATE DATABASE validaiton;
+
+-- DROP DATABASE test;
+
+-- CREATE TABLE products(
+-- 	id SERIAL PRIMARY KEY,
+-- 	name VARCHAR (50),
+-- 	department VARCHAR(50),
+-- 	price INTEGER,
+-- 	weight INTEGER
+-- );
+
+INSERT INTO products(name, department, price, weight)
+VALUES ('Shirt', 'Clothes', 20, 1);
