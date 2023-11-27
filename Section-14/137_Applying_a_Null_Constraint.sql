b412cc31a1513f3c8b8027db4c15452e3b54cd2c
alchemistbg <alchemist_bg@abv.bg>
2023-11-27 20:31:46 +0100

    Add SQL code for the lecture 'Applying a NULL Constraint'

---
+++ Section-14/137_Applying_a_Null_Constraint.sql
@@ -0,0 +1,14 @@
+-- This is all the code for this lecture
+
+-- INSERT INTO products (name, department, weight)
+-- 	VALUES ('Pants', 'Clothes', 3);
+
+-- SELECT * FROM products;
+
+-- DELETE FROM products WHERE id = 3;
+
+-- SELECT * FROM products;
+
+-- ALTER TABLE products
+-- 	ALTER COLUMN price
+-- 	SET NOT NULL;
