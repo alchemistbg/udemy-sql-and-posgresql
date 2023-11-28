d68e51214d3587cfe1970606b596986dc7b27b31
alchemistbg <alchemist_bg@abv.bg>
2023-11-28 20:12:46 +0100

    Add SQL code for the lecture 'Solving a Gotcha with NULL Constraint'

---
+++ Section-14/138_Solving_a_Gotcha_with_Null_Constraints.sql
@@ -0,0 +1,12 @@
+-- The entire code for this lecture
+
+-- UPDATE products SET price = 9999 WHERE price IS NULL;
+
+-- SELECT * FROM products;
+
+-- ALTER TABLE products
+-- 	ALTER COLUMN price
+-- 	SET NOT NULL;
+
+INSERT INTO products (name, department, weight)
+	VALUES ('Socks', 'Clothes', 1);
