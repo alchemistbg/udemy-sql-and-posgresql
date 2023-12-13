8fe621c1ea3077693c74f1f574e1c426534f6c52
alchemistbg <alchemist_bg@abv.bg>
2023-12-13 20:31:54 +0100

    Add files for the lecture 'Adding Some Data'

---
+++ Section-21/177_Adding_Some_Data.sql
@@ -0,0 +1,11 @@
+-- Testing the data after data import
+
+SELECT COUNT(*) FROM users;
+
+SELECT username from users;
+
+SELECT COUNT(*) FROM likes;
+
+SELECT COUNT(*) FROM posts;
+
+SELECT COUNT(*) FROM comments;
