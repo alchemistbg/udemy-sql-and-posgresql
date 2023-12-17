0fbd871003c94cb9bb08219ddd2533354cae6d4b
alchemistbg <alchemist_bg@abv.bg>
2023-12-17 20:52:48 +0100

    Add files for the lecture 'Creating an Index'

---
+++ Section-23/193_Creating_an_Index.sql
@@ -0,0 +1,8 @@
+-- Create index
+-- CREATE INDEX ON users(username);
+
+-- Create index with custom name
+CREATE INDEX users_username_idx ON users(username);
+
+-- Delete index
+-- DROP INDEX users_username_idx;
