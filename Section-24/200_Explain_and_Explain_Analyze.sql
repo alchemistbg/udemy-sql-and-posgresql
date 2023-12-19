0c43bb4cda936c510abaf63dee9ca6d1fe89bdc2
alchemistbg <alchemist_bg@abv.bg>
2023-12-19 20:03:16 +0100

    Add files for the lecture 'Explain and Explain Analyze'

---
+++ Section-24/200_Explain_and_Explain_Analyze.sql
@@ -0,0 +1,8 @@
+-- An example query
+-- SELECT username, contents FROM comments JOIN users ON users.id = comments.user_id WHERE username = 'Alyson14';
+
+-- Using EXPLAIN on the above query
+-- EXPLAIN SELECT username, contents FROM comments JOIN users ON users.id = comments.user_id WHERE username = 'Alyson14';
+
+-- Using EXPLAIN ANALYSE on the above query
+-- EXPLAIN ANALYSE SELECT username, contents FROM comments JOIN users ON users.id = comments.user_id WHERE username = 'Alyson14';
