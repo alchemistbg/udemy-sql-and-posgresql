0638a6a987386746da4b6975fd0ec8749301daff
alchemistbg <alchemist_bg@abv.bg>
2023-12-23 19:41:19 +0100

    Add some of the files for the lectures from the section 'Recursive Common Table Expressions'

--- Section-27/212_Recursive_CTE_Step_by_Step.sql
+++ Section-27/212_Recursive_CTE_Step_by_Step.sql
@@ -1,7 +1,9 @@
-WITH RECURSIVE countdown(val) AS (
-	SELECT 3 AS val -- Initial, Non-recursive query
-	UNION
-	SELECT val - 1 from countdown WHERE val > 1 -- Recursive query
-)
-SELECT *
-FROM countdown;
+-- WITH RECURSIVE countdown(val) AS (
+-- 	SELECT 3 AS val -- Initial, Non-recursive query
+-- 	UNION
+-- 	SELECT val - 1 from countdown WHERE val > 1 -- Recursive query
+-- )
+-- SELECT *
+-- FROM countdown;
+
+-- SELECT 3 AS val;
