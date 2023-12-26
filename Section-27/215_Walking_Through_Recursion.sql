06ff6951ed93f3d91ac93f071fc7875d01f85790
alchemistbg <alchemist_bg@abv.bg>
2023-12-26 20:03:25 +0100

    Add some edits of the files for the lectures from the section 'Recursive Common Table Expressions'

--- Section-27/215_Walking_Through_Recursion.sql
+++ Section-27/215_Walking_Through_Recursion.sql
@@ -1,7 +1,7 @@
 WITH RECURSIVE suggestions (leader_id, follower_id, depth) AS (
 	SELECT leader_id, follower_id, 1 AS depth
 	FROM followers
-	WHERE follower_id = 1000
+	WHERE follower_id = 1
 	UNION
 	SELECT followers.leader_id, followers.follower_id, depth + 1
 	FROM followers
