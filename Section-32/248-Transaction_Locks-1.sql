6d505fd281c3311dc212bf8ffa02ffd458b48b1e
alchemistbg <alchemist_bg@abv.bg>
2024-01-14 20:32:26 +0100

    Add the code for the lecture 'Transaction Locks'

---
+++ Section-32/248-Transaction_Locks-1.sql
@@ -0,0 +1,11 @@
+-- SELECT * FROM posts;
+
+-- BEGIN;
+
+-- UPDATE posts
+-- SET lat = 42.42
+-- WHERE id = 1
+
+-- SELECT * FROM posts;
+
+COMMIT;
---
+++ Section-32/248-Transaction_Locks-2.sql
@@ -0,0 +1,7 @@
+-- SELECT * FROM posts;
+
+-- UPDATE posts
+-- SET lat = 84.84
+-- WHERE id = 1
+
+SELECT * FROM posts;
