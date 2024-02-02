d3b327c74bcf38ec99d14e1e2558236c7f4a0360
alchemistbg <alchemist_bg@abv.bg>
2024-02-02 20:00:06 +0100

    Add code for lecture 'Creating and Accessing Schemas'

---
+++ Section-36/281_Creating_and_Accessing_Schemas.sql
@@ -0,0 +1,14 @@
+-- CREATE SCHEMA test;
+
+-- CREATE TABLE test.users(
+-- 	id SERIAL PRIMARY KEY,
+-- 	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+-- 	updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+-- 	username VARCHAR(20) NOT NULL,
+-- 	bio VARCHAR(300);
+-- );
+
+-- INSERT INTO test.users (username, bio)
+-- VALUES ('some test user', 'some bio');
+
+-- SELECT * FROM test.users;
