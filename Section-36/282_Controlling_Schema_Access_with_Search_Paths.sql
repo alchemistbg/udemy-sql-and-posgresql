8eb7bf6ca2d8cf380b89c249b144a58d35e074b1
alchemistbg <alchemist_bg@abv.bg>
2024-02-04 12:54:58 +0100

    Add code for lecture 'Controlling Schema Access with Search Paths'

---
+++ Section-36/282_Controlling_Schema_Access_with_Search_Paths.sql
@@ -0,0 +1,13 @@
+-- SHOW search_path;
+
+-- SET search_path TO test, public;
+
+-- SHOW search_path;
+
+-- SELECT * FROM users;
+
+-- SELECT * FROM PUBLIC.users;
+
+-- SET search_path TO "$user", public;
+
+-- SHOW search_path;
